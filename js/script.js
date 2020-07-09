6f7041490aa8903cd26143cddf35130537447a6b
prashathc <prashathc@gmail.com>
2020-07-09 15:30:35 -0400

    initial

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// All animations will take exactly 500ms
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 500,
+  speedAsDuration: true,
+  offset: 114.5,
+});
+
+// Mobile navbar
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("menu-items")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+const menuItems = Array.from(document.querySelectorAll(".menu-items a"));
+
+menuItems.forEach((item) =>
+  item.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+  })
+);
